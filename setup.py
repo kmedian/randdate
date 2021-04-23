2a13dfc27bf7694aba54459f765c1b2c18e3ae7f
UH <554c46@gmail.com>
2021-04-24 00:58:24 +0200

    remove pandoc from setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,10 @@
 from setuptools import setup
 
 
-def read(fname):
-    import os
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-
 setup(name='randdate',
       version='0.1.0',
       description='Generate a list of random dates or resp. datetime objects',
-      long_description=read('README.md'),
+      long_description='README.rst',
       long_description_content_type='text/markdown',
       url='http://github.com/kmedian/randdate',
       author='Ulf Hamster',
@@ -18,4 +13,4 @@ setup(name='randdate',
       packages=['randdate'],
       # install_requires=[],
       python_requires='>=3.6',
-      zip_safe=False)
+      zip_safe=True)
