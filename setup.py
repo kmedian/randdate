7a2fb1752570c1c64eff3979ada26503e9311008
UH <554c46@gmail.com>
2021-04-23 21:42:16 +0200

    setuptools removed

--- setup.py
+++ setup.py
@@ -14,9 +14,8 @@ setup(name='randdate',
       url='http://github.com/kmedian/randdate',
       author='Ulf Hamster',
       author_email='554c46@gmail.com',
-      license='MIT',
+      license='Apache License 2.0',
       packages=['randdate'],
-      install_requires=[
-          'setuptools>=40.0.0'],
+      # install_requires=[],
       python_requires='>=3.6',
       zip_safe=False)
