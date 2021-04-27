e9e01789a52ad07999574020eb04f783bd40e375
UH <554c46@gmail.com>
2021-04-27 18:57:31 +0200

    test more py versions, badges added, read rst file

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup
+import os
+
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as fp:
+        s = fp.read()
+    return s
 
 
 setup(name='randdate',
       version='0.1.0',
       description='Generate a list of random dates or resp. datetime objects',
-      long_description='README.rst',
+      long_description=read('README.rst'),
       long_description_content_type='text/markdown',
       url='http://github.com/kmedian/randdate',
       author='Ulf Hamster',
